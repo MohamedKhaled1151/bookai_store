fd356bf4312c559fa5f194c0e9d591976cec9a8b
Mohamedkhaled151 <mohamedkhaled151@gmail.com>
2025-09-26 15:09:38 +0300

    create app

---
+++ lib/bookia_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookiaApp extends StatelessWidget {
+  const BookiaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
